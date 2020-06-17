962c4e5e3a723a2b6950c9b7da980a17fad967ff
liuzheng-arctic <liuzheng.atmos@gmail.com>
2020-06-17 02:55:38 +0000

    Add overcast folder to host our functions and a setup.py to access them easily. A notebook is added to collocate ATL07 and ATL09 data together.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(name='overcast',
+      version='0.1.0',
+      description='ICESat-2 data packages for the study of leads and clouds',
+      license='MIT',
+      author='IS2 Hackweek overcast group',
+      author_email='liuzheng@apl.uw.edu',
+      packages=find_packages(),
+     )
